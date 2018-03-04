bfb16eaaf5652704504b22e98f97f967ab2ea98e
Brady Helkenn <bhelkenn@gmail.com>
2018-03-03 19:29:25 -0800

    got stuck with HW3, need more lecture

---
+++ HW3/relationships.pl
@@ -0,0 +1,7 @@
+/* lists */
+m([brady, john, will, charles, richard, frank, william, helkenn]).
+f([nadine, patricia, rose, beers]).
+family([FATHER, MOTHER, [CHILD1, CHILD2, CHILD3]]).
+
+/* rules */
+
