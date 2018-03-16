1a8c7087d6c1e9630897e70e5f860e9c75ab94d2
Brady Helkenn <bhelkenn@gmail.com>
2018-03-15 20:05:53 -0700

    finished HW3

--- HW3/8queens.pl
+++ HW3/8queens.pl
@@ -1,6 +1,9 @@
 valid_range([1,2,3,4,5,6,7,8]).
+queens([1/Y1,2/Y2,3/Y3,4/Y4,5/Y5,6/Y6,7/Y7,8/Y8]).
 
 /* rules */
+list_solutions(Queens) :- queens(Queens), solutions(Queens).
+
 solutions([]).
 solutions([X/Y|Queens]) :- solutions(Queens), legal(Y), nocheck(X/Y, Queens).
 
@@ -14,5 +17,3 @@ member_of(X, [X|_]).
 member_of(X, [_|T]) :- member_of(X, T).
 
 legal(Y) :- valid_range(R), member_of(Y, R).
-
-queens([1/Y1,2/Y2,3/Y3,4/Y4,5/Y5,6/Y6,7/Y7,8/Y8]).
